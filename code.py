21c9fd896199c836e226b907d2bcd0dbe8613e6a
TateSummers <104160774+TateSummers@users.noreply.github.com>
2022-05-12 11:28:08 -0700

    Update code.py

--- code.py
+++ code.py
@@ -21,43 +21,26 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 WHITE = (255,255,255)
 OFF = (0,0,0)
+ORANGE = (180,20,0)
 
-# helper function for fading - do not touch
-def fade(c1, c2):
-    diff = (c2[0]-c1[0], c2[1]-c1[1], c2[2]-c1[2])
-    diffA = [1 if n==0 else abs(n) for n in diff]
-    maxDiff = max(diffA)
-    index = diffA.index(maxDiff)
-    cFade = list(c1)
-    increment = [int(diff[i]/diffA[i]) for i in range(3)]
-    for i in range(0, maxDiff):
-        for n in range(3):
-            if(cFade[n] != c2[n]):
-                cFade[n] += increment[n]
-        pixels.fill(tuple(cFade))
-        pixels.show()
-        time.sleep(0.01)  # debounce delay
-#end helper function
 now = 0
 
 while True:
     if touch.value:
         now=now+1
+        print(now)
         if (now == 0):
             pixels.fill(OFF)
             pixels.show()
         if (now == 1):
-            fade(OFF,RED)
-            fade(RED,PURPLE)
-            fade(PURPLE,BLUE)
-            fade(BLUE,CYAN)
-            fade(CYAN,GREEN)
-            fade(GREEN,WHITE)
-            fade(WHITE,OFF)
-            time.sleep(0.5)
-        if touch.value:
-            now = now+1
-            break
+            COLOR = BLUE
+            for i in range(0,12,1):
+                pixels[i] = COLOR
+                pixels.show()
+                time.sleep(0.1)
+            if touch.value:
+                now = now+1
+                break
         if (now == 2):
             COLOR = PURPLE
             for i in range(0,12,1):
@@ -76,6 +59,32 @@ while True:
             if touch.value:
                 now = now+1
                 break
-    if(now>3):
+        if (now == 4):
+            COLOR = ORANGE
+            for i in range(0,12,1):
+                pixels[i] = COLOR
+                pixels.show()
+                time.sleep(0.1)
+            if touch.value:
+                now = now+1
+                break
+        if (now == 5):
+            COLOR = YELLOW
+            for i in range(0,12,1):
+                pixels[i] = COLOR
+                pixels.show()
+                time.sleep(0.1)
+            if touch.value:
+                now = now+1
+                break
+        if (now == 6):
+            COLOR = WHITE
+            for i in range(0,12,1):
+                pixels[i] = COLOR
+                pixels.show()
+                time.sleep(0.1)
+            if touc.value:
+                now = now+1
+                break
+    if(now>6):
         now = 0
-    time.sleep(0.2)
